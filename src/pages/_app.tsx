46ab2d76d7fb16de53d94acb919a3e5a18b66198
goohyunju <mcrc890@gmail.com>
2023-08-25 17:37:21 +0900

    파일구조 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,24 @@
-import '@/styles/globals.css';
-import { ApolloClient, ApolloProvider } from '@apollo/client';
+import '../styles/globals.css'; // 상대 경로로 스타일 시트를 가져옵니다.
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import type { AppProps } from 'next/app';
-import client from '../lib/hasura';
 
+const authToken = 'YOUR_AUTH_TOKEN'; // Hasura나 다른 인증 방식에 따라서 토큰을 설정합니다.
 
-const authToken = 'YOUR_AUTH_TOKEN';
-const client = createApolloClient(authToken);
+// Apollo Client를 생성합니다.
+const client = new ApolloClient({
+  uri: 'https://hana-donkey.hasura.app/v1/graphql', // Hasura GraphQL 엔드포인트 URL
+  cache: new InMemoryCache(),
+  headers: {
+    Authorization: `Bearer ${authToken}`, // 인증 토큰을 헤더에 추가합니다.
+  },
+});
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={polloClient}>
-      <Component {...pageProps}>
-      </Component>
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
     </ApolloProvider>
   );
 }
+
+export default MyApp;
